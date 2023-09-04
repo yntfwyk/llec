697e0fa88d4f3662bfe427e4385debaad8c6c31c
yntfwyk <yntfwyk@gmail.com>
2023-09-04 16:05:55 -0500

    option for compile-time strings

--- include/llec/utility/checksum.hpp
+++ include/llec/utility/checksum.hpp
@@ -5,6 +5,7 @@
 
 #pragma once
 #include "../core/core.hpp"
+#include <string_view>
 
 namespace llec::checksum
 {
@@ -12,7 +13,7 @@ namespace llec::checksum
     /// @param data to get a checksum of
     /// @param length in bytes
     /// @return checksum of type u32
-    constexpr u32 adler32(const void* data, std::size_t length)
+    constexpr u32 adler32(const void* data, std::size_t length) noexcept
     {
         constexpr u32 ADLER_MAGIC = 65521u;
         const u8* bytes = static_cast<const u8*>(data);
@@ -25,6 +26,23 @@ namespace llec::checksum
         return (b << 16u) | a;
     }
 
+    /// @brief adler32
+    /// @param compile-time string to get a checksum of
+    /// @return checksum of type u32
+    constexpr u32 adler32(std::string_view str) noexcept
+    {
+        constexpr u32 ADLER_MAGIC = 65521u;
+        const std::size_t length = str.length();
+        u32 a = 1u, b = 0u;
+        for (std::size_t i = 0u; i < length; i++)
+        {
+            a = (a + str[i]) % ADLER_MAGIC;
+            b = (b + a) % ADLER_MAGIC;
+        }
+        return (b << 16u) | a;
+    }
+
+
     /// @brief fnv1a (32 or 64)
     /// @tparam T u32 or u64
     /// @param data to get a hash of
@@ -32,7 +50,7 @@ namespace llec::checksum
     /// @return hash
     template <typename T = u32>
         requires traits::u32_64_integral<T>
-    constexpr T fnv1a(const void* data, std::size_t length)
+    constexpr T fnv1a(const void* data, std::size_t length) noexcept
     {
         T hash = traits::is_u64_v<T> ? 0xCBF29CE484222325u : 0x811C9DC5u;
         constexpr T fnv_prime = traits::is_u64_v<T> ? 0x100000001B3u : 0x01000193u;
@@ -46,23 +64,40 @@ namespace llec::checksum
         return hash;
     }
 
+    /// @brief fnv1a (32 or 64) for compile-time strings
+    /// @tparam T u32 or u64
+    /// @param compile-time string to get a hash of
+    /// @return hash
+    template <typename T = u32>
+        requires traits::u32_64_integral<T>
+    constexpr T fnv1a(std::string_view str) noexcept
+    {
+        T hash = traits::is_u64_v<T> ? 0xCBF29CE484222325u : 0x811C9DC5u;
+        constexpr T fnv_prime = traits::is_u64_v<T> ? 0x100000001B3u : 0x01000193u;
+        const std::size_t length = str.length();
+        for (std::size_t i = 0u; i < length; i++)
+        {
+            hash = hash ^ str[i];
+            hash = hash * fnv_prime;
+        }
+        return hash;
+    }
+
+
 } // namespace llec::checksum
 
 constexpr llec::u32 operator""_adler(const char* str, [[maybe_unused]] std::size_t len) noexcept
 {
-    const std::string_view sv{str};
-    return llec::checksum::adler32(sv.data(), sv.length());
+    return llec::checksum::adler32(str);
 }
 
 constexpr llec::u32 operator""_fnv32(const char* str, [[maybe_unused]] std::size_t len) noexcept
 {
-    const std::string_view sv{str};
-    return llec::checksum::fnv1a(sv.data(), sv.length());
+    return llec::checksum::fnv1a(str);
 }
 
 constexpr llec::u64 operator""_fnv64(const char* str, [[maybe_unused]] std::size_t len) noexcept
 {
-    const std::string_view sv{str};
-    return llec::checksum::fnv1a<llec::u64>(sv.data(), sv.length());
+    return llec::checksum::fnv1a<llec::u64>(str);
 }
 
--- tests/checksum_tests.cc
+++ tests/checksum_tests.cc
@@ -6,10 +6,10 @@ TEST_CASE("Adler32", "[checksum][adler32]")
 {
     CHECK("llec"_adler == 0x042501A1u);
 
-    llec::u32 number = 9u;
+    constexpr llec::u32 number = 9u;
     CHECK(llec::checksum::adler32(&number, sizeof(llec::u32)) == 0x0028000Au);
 
-    llec::f32 fnumber = 0.99556644f;
+    constexpr llec::f32 fnumber = 0.99556644f;
     CHECK(llec::checksum::adler32(&fnumber, sizeof(llec::f32)) == 0x059A020Cu);
 }
 
@@ -17,10 +17,10 @@ TEST_CASE("FNV1a32", "[checksum][fnv1a]")
 {
     CHECK("llec"_fnv32 == 0x6EA3999u);
 
-    llec::u32 number = 9u;
+    constexpr llec::u32 number = 9u;
     CHECK(llec::checksum::fnv1a(&number, sizeof(llec::u32)) == 0x7CCBAE8Cu);
 
-    llec::f32 fnumber = 0.99556644f;
+    constexpr llec::f32 fnumber = 0.99556644f;
     CHECK(llec::checksum::fnv1a(&fnumber, sizeof(llec::f32)) == 0x7DDCD278u);
 }
 
@@ -28,9 +28,9 @@ TEST_CASE("FNV1a64", "[checksum][fnv1a]")
 {
     CHECK("llec"_fnv64 == 0xEA8340AD81C318D9u);
 
-    llec::u32 number = 9u;
+    constexpr llec::u32 number = 9u;
     CHECK(llec::checksum::fnv1a<llec::u64>(&number, sizeof(llec::u32)) == 0xAD002AB9F9463BECu);
 
-    llec::f32 fnumber = 0.99556644f;
+    constexpr llec::f32 fnumber = 0.99556644f;
     CHECK(llec::checksum::fnv1a<llec::u64>(&fnumber, sizeof(llec::f32)) == 0x976B0005B2C623D8u);
 }
